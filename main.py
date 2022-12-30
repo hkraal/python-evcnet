feed9600e5879cc6bd2232223c6eacb84d895a87
Henk Kraal <hkraal@users.noreply.github.com>
2022-12-30 12:14:56 +0100

    Use env variables

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
+import os
+
+from dotenv import load_dotenv
+
 from evcnet import Evcnet
+
+load_dotenv()
 
 e = Evcnet(
     url='https://evcompany.evc-net.com',
-    username='h.kraal@exonet.nl',
-    password='secret',
+    username=os.environ.get('EVCNET_USERNAME'),
+    password=os.environ.get('EVCNET_PASSWORD'),
 )
 data = e.total_usage()
+print(data)
--- tests/test_evcnet.py
+++ tests/test_evcnet.py
@@ -1,11 +1,13 @@
+import os
+
 from evcnet import Evcnet
 
 
 def test_total_usage():
     e = Evcnet(
         url='https://evcompany.evc-net.com',
-        username='h.kraal@exonet.nl',
-        password='secret',
+        username=os.environ.get('EVCNET_USERNAME'),
+        password=os.environ.get('EVCNET_PASSWORD'),
     )
     data = e.total_usage()
     assert 'totalUsage' in data.keys()
